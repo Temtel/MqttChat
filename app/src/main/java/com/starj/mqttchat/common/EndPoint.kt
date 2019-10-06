40605e9e357cf61834a428de2707da848f5fb474
Temtel <temtel@gmail.com>
2019-10-05 20:40:15 -0700

    IP of MQTT

--- app/src/main/java/com/starj/mqttchat/common/EndPoint.kt
+++ app/src/main/java/com/starj/mqttchat/common/EndPoint.kt
@@ -3,6 +3,6 @@ package com.starj.mqttchat.common
 
 object EndPoint {
     // TODO : Set EndPoint of MQTT Broker to yours
-    const val ENDPOINT_MQTT_BROKER = "tcp://packetchat.com:1883"
+    const val ENDPOINT_MQTT_BROKER = "tcp://192.168.29.241:1883"
 
 }
